058e4567d76d0e5196023e0c24a6f6533aeb84b7
anque <anquetilaugustin@gmail.com>
2023-05-31 15:38:17 +0200

    grrrrr

--- php/class/User.php
+++ php/class/User.php
@@ -3,7 +3,7 @@
 class User
 {
     // Récupère l'id de l'utilisateur à partir de son mail
-    public static function id($mail_user) {
+    public static function id_usr($mail_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT id_user FROM user WHERE mail_user = :mail_user';
@@ -19,7 +19,7 @@ class User
     }
 
     // Récupère le nom de l'utilisateur à partir de son identifiant
-    public static function nom($id_user) {
+    public static function nom_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT nom_user FROM user WHERE id_user = :id_user';
@@ -35,7 +35,7 @@ class User
     }
 
     // Récupère le prénom de l'utilisateur à partir de son identifiant
-    public static function prenom($id_user) {
+    public static function prenom_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT prenom_user FROM user WHERE id_user = :id_user';
@@ -51,7 +51,7 @@ class User
     }
 
     // Récupère l'âge' de l'utilisateur à partir de son identifiant
-    public static function age($id_user) {
+    public static function age_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT age_user FROM user WHERE id_user = :id_user';
@@ -67,7 +67,7 @@ class User
     }
 
     // Récupère le mdp de l'utilisateur à partir de son identifiant
-    public static function mdp($id_user) {
+    public static function mdp_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT mdp_user FROM user WHERE id_user = :id_user';
@@ -83,7 +83,7 @@ class User
     }
 
     // Récupère le pseudo de l'utilisateur à partir de son identifiant
-    public static function pseudo($id_user) {
+    public static function pseudo_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT pseudo_user FROM user WHERE id_user = :id_user';
@@ -99,7 +99,7 @@ class User
     }
 
     // Récupère le photo de l'utilisateur à partir de son identifiant
-    public static function photo($id_user) {
+    public static function photo_usr($id_user) {
         try {
             $conn = spotvi::connexionBD();
             $sql = 'SELECT photo_user FROM user WHERE id_user = :id_user';
