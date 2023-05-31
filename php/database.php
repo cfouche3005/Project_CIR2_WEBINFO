8aa059adbc94763a77db3e29b3bc5caca655ecd1
Paul Kebci <paul.kebci@isen-ouest.yncrea.fr>
2023-05-31 14:37:54 +0200

    fonction dbConnect

--- php/database.php
+++ php/database.php
@@ -1,1 +1,17 @@
 <?php
+
+function dbConnect() //fonction de connexion à la base de données
+  {
+    try
+    {
+      $db = new PDO('pgsql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Connection error: '.$exception->getMessage());
+      return false;
+    }
+    return $db;
+  }
+
