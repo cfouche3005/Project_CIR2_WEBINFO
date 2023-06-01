3d42c27c1b80055bf20e94d6669ad8dc04b946dd
anque <anquetilaugustin@gmail.com>
2023-06-01 11:21:46 +0200

    add gitignore

--- php/class/Album.php
+++ php/class/Album.php
@@ -1,15 +1,17 @@
 <?php
-
+require_once ('database.php');
 class Album
 {
     // Récupère le nom de tous les albums
     public static function nom() {
         try {
-            $conn = spotvi::connexionBD();
-            $sql = 'SELECT nom_album FROM album ';
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $conn = dbConnect();
+            if($conn){
+                $sql = 'SELECT nom_album FROM album';
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
         } catch (PDOException $exception) {
             error_log('Connection error: ' . $exception->getMessage());
             return false;
@@ -20,7 +22,7 @@ class Album
     // Récupère l'id de l'album à partir de son nom
     public static function id_alb($nom_album) {
         try {
-            $conn = spotvi::connexionBD();
+            $conn = dbConnect();
             $sql = 'SELECT id_album FROM album WHERE nom_album = :nom_album';
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':nom_album', $nom_album);
@@ -36,7 +38,7 @@ class Album
     // Récupère la date de l'album à partir de son id
     public static function date_alb($id_album) {
         try {
-            $conn = spotvi::connexionBD();
+            $conn = dbConnect();
             $sql = 'SELECT date_album FROM album WHERE id_album = :id_album';
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id_album', $id_album);
@@ -52,7 +54,7 @@ class Album
     // Récupère l'image de l'album à partir de son id
     public static function image_alb($id_album) {
         try {
-            $conn = spotvi::connexionBD();
+            $conn = dbConnect();
             $sql = 'SELECT image_album FROM album WHERE id_album = :id_album';
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id_album', $id_album);
@@ -68,7 +70,7 @@ class Album
     // Récupère le type de l'album à partir de son id
     public static function type_alb($id_album) {
         try {
-            $conn = spotvi::connexionBD();
+            $conn = dbConnect();
             $sql = 'SELECT type_album FROM album WHERE id_album = :id_album';
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id_album', $id_album);
